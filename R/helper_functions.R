09735a97632ceb3db8ddde0d1f5222635721c302
Richard Aubrey White <hello@rwhite.no>
2023-12-21 11:43:57 +0100

    helper functions

---
+++ R/helper_functions.R
@@ -0,0 +1,29 @@
+#' @export
+min_with_infinite_as_na <- function(x, na.rm=T){
+  suppressWarnings(retval <- min(x, na.rm=na.rm))
+  retval[is.infinite(retval)] <- NA
+  return(retval)
+}
+
+#' @export
+max_with_infinite_as_na <- function(x, na.rm=T){
+  suppressWarnings(retval <- max(x, na.rm=na.rm))
+  retval[is.infinite(retval)] <- NA
+  return(retval)
+}
+
+#' @export
+as_logical_min_with_infinite_as_na <- function(x, na.rm=T){
+  as.logical(min_with_infinite_as_na(x, na.rm=na.rm))
+}
+
+#' @export
+as_logical_max_with_infinite_as_na <- function(x, na.rm=T){
+  as.logical(max_with_infinite_as_na(x, na.rm=na.rm))
+}
+
+#' @export
+first_non_na <- function(x){
+  dplyr::first(na.omit(x))
+}
+
